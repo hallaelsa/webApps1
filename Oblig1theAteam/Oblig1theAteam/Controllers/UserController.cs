92b567cbdfc23360138db9e10687631bdafe6a6c
Lars <reelars@gmail.com>
2018-09-20 11:32:51 +0200

    begynt på user og userservice

---
+++ Oblig1theAteam/Oblig1theAteam/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oblig1theAteam.Business.Users;
+using Oblig1theAteam.ViewModels.User;
+
+namespace Oblig1theAteam.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserService userService;
+
+        public UserController(UserController userService)
+        {
+            this.userService = userService;
+        }
+
+        public ActionResult Registrer()
+        {
+            return View();
+        }   
+
+        [HttpPost]
+        public IActionResult Index()//string email)
+        {
+            // View modellen skal ha samme navn som metoden + "ViewModel".
+            var model = new IndexViewModel();
+            //model.User = userService.Get(1);
+            //model.Orders = orderService.ListByUser(model.User.Id);
+            model.User = userService.GetUser("fiskebolle@gmail.com");
+
+            return View(model);
+        }
+
+
+    }
+
+
+
+
+}
---
+++ Oblig1theAteam/Oblig1theAteam/ViewModels/User/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oblig1theAteam.ViewModels.User
+{
+    public class IndexViewModel
+    {
+        public List<Business.Users.Models.User> User { get; set; }
+    }
+}
