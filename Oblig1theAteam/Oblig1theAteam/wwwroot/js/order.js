7a3a04995922aef41d9358c0f7ba6e5b59f9537f
evannes <elisevannes@outlook.com>
2018-09-25 10:44:24 +0200

    cm

---
+++ Oblig1theAteam/Oblig1theAteam/wwwroot/js/order.js
@@ -0,0 +1,29 @@
+﻿function showVisaForm() {
+    jQuery("#visa-form").css("visibility", "visible");
+
+}
+
+$(function () {
+    $('#editCourseModal').on("click", function (e) {
+        e.preventDefault();
+        console.log("halla")
+        //perform the url load  then
+        $('#orderModal').modal({
+            keyboard: true
+        }, 'show');
+        return false;
+    })
+})
+
+$(document).ready(function () {
+    $('a[data-movieModal=true]').click(function () {
+        const movie_id = $(this).data('movie-id');
+        $('#modal-home-button').click(function () {
+            window.location.replace("/");
+        });
+        const movie_id = $(this).data('movie-id');
+        $('#modal-order-page-button').click(function () {
+            window.location.replace("/Order/MyOrders");
+        });
+    })
+});
