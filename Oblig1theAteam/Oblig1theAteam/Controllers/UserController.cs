045f5553429510b107f99eabf0a5b6c0374b15c7
miina <hallaelsa@hotmail.com>
2018-09-27 13:08:03 +0200

    backup

--- Oblig1theAteam/Oblig1theAteam/Controllers/UserController.cs
+++ Oblig1theAteam/Oblig1theAteam/Controllers/UserController.cs
@@ -16,19 +16,6 @@ namespace Oblig1theAteam.Controllers
         {
             this.userService = userService;
         }
-
-        [HttpPost]
-        public IActionResult Index()//string email)
-        {
-            // View modellen skal ha samme navn som metoden + "ViewModel".
-            var model = new IndexViewModel();
-            //model.User = userService.Get(1);
-            //model.Orders = orderService.ListByUser(model.User.Id);
-            model.User = userService.GetUser("fiskebolle@gmail.com");
-
-            return View(model);
-        }
-
         
         public IActionResult Register()
         {
